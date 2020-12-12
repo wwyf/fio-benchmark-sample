49e12a3d99b9a24387fd02be99d04b952d034515
wwyf <w.y.ff@163.com>
2020-12-12 16:22:53 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://docs.cloud.oracle.com/en-us/iaas/Content/Block/References/samplefiocommandslinux.htm
+https://docs.virtuozzo.com/virtuozzo_infrastructure_platform_3_benchmarking_guide/appendix/randwrite.html
--- bench.sh
+++ bench.sh
@@ -1,71 +1,169 @@
 #!/bin/bash
 
 set -x
+current=`date "+%Y-%m-%d-%H-%M-%S"`
+LOGPATH=logs/${current}
+mkdir -p ${LOGPATH}
 
-# devs=(/mnt/nvme/test-kljasnoi)
-names=(HDD SSD NVMe)
-devs=(/dev/sda /dev/sdb  /dev/nvme0n1)
 
-for dev in ${devs[*]}
+function fio_latency_randr {
+    bench_name=latency.randr
+    store_type=$1
+    dev_name=$2
+    log_path=$3
+
+    fio --filename=${dev_name} \
+    --rw=randread \
+    --bs=4k \
+    --ioengine=libaio \
+    --iodepth=1 \
+    --numjobs=1 \
+    --direct=1 \
+    --time_based --group_reporting --runtime=120 --eta-newline=1 --readonly \
+    --name=${bench_name} | tee  ${log_path}/${store_type}.${bench_name}.log
+    latency=`cat ${log_path}/${store_type}.${bench_name}.log | grep -P ' +lat.*avg=(\d+.\d+),.*' -o | awk '{print $5}' | grep -P '\d+\.\d+' -o `
+    echo ${bench_name},${store_type},${dev_name},${latency} >> ${log_path}/results.csv
+}
+
+function fio_latency_randw {
+    bench_name=latency.randw
+    store_type=$1
+    dev_name=$2
+    log_path=$3
+
+    fio --filename=${dev_name} \
+    --rw=randwrite \
+    --bs=4k \
+    --ioengine=libaio \
+    --iodepth=1 \
+    --numjobs=1 \
+    --direct=1 \
+    --time_based --group_reporting --runtime=120 --eta-newline=1 \
+    --name=${bench_name} | tee  ${log_path}/${store_type}.${bench_name}.log
+    latency=`cat ${log_path}/${store_type}.${bench_name}.log | grep -P ' +lat.*avg=(\d+.\d+),.*' -o | awk '{print $5}' | grep -P '\d+\.\d+' -o `
+    echo ${bench_name},${store_type},${dev_name},${latency} >> ${log_path}/results.csv
+}
+
+function fio_latency_randrw {
+    bench_name=latency.randrw
+    store_type=$1
+    dev_name=$2
+    log_path=$3
+
+    fio --filename=${dev_name} \
+    --rw=randrw \
+    --bs=4k \
+    --ioengine=libaio \
+    --iodepth=1 \
+    --numjobs=1 \
+    --direct=1 \
+    --time_based --group_reporting --runtime=120 --eta-newline=1 \
+    --name=${bench_name} | tee  ${log_path}/${store_type}.${bench_name}.log
+    latency=`cat ${log_path}/${store_type}.${bench_name}.log | grep -P ' +lat.*avg=(\d+.\d+),.*' -o | awk '{print $5}' | grep -P '\d+\.\d+' -o `
+    echo ${bench_name},${store_type},${dev_name},${latency} >> ${log_path}/results.csv
+}
+
+
+function fio_bandwidth_randr {
+    bench_name=bandwidth.randr
+    store_type=$1
+    dev_name=$2
+    log_path=$3
+
+    fio --filename=${dev_name} \
+    --rw=randread \
+    --bs=64k \
+    --ioengine=libaio \
+    --iodepth=64 \
+    --numjobs=4 \
+    --direct=1 \
+    --time_based --group_reporting --runtime=120 --eta-newline=1 --readonly \
+    --name=${bench_name} | tee  ${log_path}/${store_type}.${bench_name}.log
+    bandwidth=`cat ${log_path}/${store_type}.${bench_name}.log | grep "IOPS" | grep -P '(?<=MiB/s \()\d+\.*\d+.*/s' -o`
+    echo ${bench_name},${store_type},${dev_name},${bandwidth} >> ${log_path}/results.csv
+}
+
+function fio_bandwidth_randw {
+    bench_name=bandwidth.randw
+    store_type=$1
+    dev_name=$2
+    log_path=$3
+
+    fio --filename=${dev_name} \
+    --rw=randwrite \
+    --bs=64k \
+    --ioengine=libaio \
+    --iodepth=64 \
+    --numjobs=4 \
+    --direct=1 \
+    --time_based --group_reporting --runtime=120 --eta-newline=1 \
+    --name=${bench_name} | tee  ${log_path}/${store_type}.${bench_name}.log
+    bandwidth=`cat ${log_path}/${store_type}.${bench_name}.log | grep "IOPS" | grep -P '(?<=MiB/s \()\d+\.*\d+.*/s' -o`
+    echo ${bench_name},${store_type},${dev_name},${bandwidth} >> ${log_path}/results.csv
+}
+
+function fio_throughput_randr {
+    bench_name=throughput.randr
+    store_type=$1
+    dev_name=$2
+    log_path=$3
+
+    fio --filename=${dev_name} \
+    --rw=randread \
+    --bs=4k \
+    --ioengine=libaio \
+    --iodepth=256 \
+    --numjobs=4 \
+    --direct=1 \
+    --time_based --group_reporting --runtime=120 --eta-newline=1 --readonly \
+    --name=${bench_name} | tee  ${log_path}/${store_type}.${bench_name}.log
+    tp=`cat cat ${log_path}/${store_type}.${bench_name}.log| grep "IOPS" | grep -P '(?<=IOPS=)\d+\.*\d+k*' -o`
+    echo ${bench_name},${store_type},${dev_name},${tp} >> ${log_path}/results.csv
+
+}
+
+function fio_throughput_randw {
+    bench_name=throughput.randw
+    store_type=$1
+    dev_name=$2
+    log_path=$3
+
+    fio --filename=${dev_name} \
+    --rw=randwrite \
+    --bs=4k \
+    --ioengine=libaio \
+    --iodepth=256 \
+    --numjobs=4 \
+    --direct=1 \
+    --time_based --group_reporting --runtime=120 --eta-newline=1 \
+    --name=${bench_name} | tee  ${log_path}/${store_type}.${bench_name}.log
+    tp=`cat cat ${log_path}/${store_type}.${bench_name}.log| grep "IOPS" | grep -P '(?<=IOPS=)\d+\.*\d+k*' -o`
+    echo ${bench_name},${store_type},${dev_name},${tp} >> ${log_path}/results.csv
+}
+
+
+# # devs=(/mnt/nvme/test-kljasnoi)
+# names=(HDD SSD NVMe)
+# devs=(/dev/sdb  /dev/nvme0n1)
+
+store_index=(0 1 2)
+store_types=(NVME_SSD SSD HDD)
+devs=( /dev/nvme0n1 /dev/sdb /dev/sda  )
+devs=( /dev/nvme0n1)
+
+
+metrics=(throughput bandwidth latency )
+# metrics=(bandwidth throughput latency )
+
+bench_types=(randw randr)
+
+for bench_type in ${bench_types}
 do
-
-# fio latency/fiorandomreadlatency.fio
-fio --filename=${dev} --direct=1 --rw=randread --bs=4k --ioengine=libaio --iodepth=1 --numjobs=1 --time_based --group_reporting --name=readlatency-test-job --runtime=120 --eta-newline=1 --readonly
-sync
-echo 3 > /proc/sys/vm/drop_caches
-sleep 5
-
-
-# fio latency/fiorandomrwlatency.fio
-fio --filename=${dev} --direct=1 --rw=randrw --bs=4k --ioengine=libaio --iodepth=1 --numjobs=1 --time_based --group_reporting --name=rwlatency-test-job --runtime=120 --eta-newline=1 
-sync
-echo 3 > /proc/sys/vm/drop_caches
-sleep 5
-
-# fio bandwidth/fiorandomread.fio
-fio --filename=${dev} --direct=1 --rw=randread --bs=64k --ioengine=libaio --iodepth=64 --runtime=120 --numjobs=4 --time_based --group_reporting --name=throughput-test-job --eta-newline=1 --readonly
-
-sync
-echo 3 > /proc/sys/vm/drop_caches
-sleep 5
-
-# fio bandwidth/fiorandomread.fio
-fio --filename=${dev} --direct=1 --rw=randrw --bs=64k --ioengine=libaio --iodepth=64 --runtime=120 --numjobs=4 --time_based --group_reporting --name=throughput-test-job --eta-newline=1
-
-sync
-echo 3 > /proc/sys/vm/drop_caches
-sleep 5
-
-# fio bandwidth/fioread.fio
-fio --filename=${dev} --direct=1 --rw=read --bs=64k --ioengine=libaio --iodepth=64 --runtime=120 --numjobs=4 --time_based --group_reporting --name=throughput-test-job --eta-newline=1 --readonly
-
-sync
-echo 3 > /proc/sys/vm/drop_caches
-sleep 5
-
-
-
-# fio iops/fiorandomread.fio
-fio --filename=${dev} --direct=1 --rw=randread --bs=4k --ioengine=libaio --iodepth=256 --runtime=120 --numjobs=4 --time_based --group_reporting --name=iops-test-job --eta-newline=1 --readonly
-
-sync
-echo 3 > /proc/sys/vm/drop_caches
-sleep 5
-
-# fio iops/fiorandomreadwrite.fio
-fio --filename=${dev} --direct=1 --rw=randrw --bs=4k --ioengine=libaio --iodepth=256 --runtime=120 --numjobs=4 --time_based --group_reporting --name=iops-test-job --eta-newline=1
-sleep 5
-
-sync
-echo 3 > /proc/sys/vm/drop_caches
-sleep 5
-
-# fio iops/fioread.fio
-fio --filename=${dev} --direct=1 --rw=read --bs=4k --ioengine=libaio --iodepth=256 --runtime=120 --numjobs=4 --time_based --group_reporting --name=iops-test-job --eta-newline=1 --readonly
-
-sync
-echo 3 > /proc/sys/vm/drop_caches
-sleep 5
-
-
+    for metric in ${metrics[*]}
+    do
+        for dev_id in ${store_index[*]}
+        do
+            fio_${metric}_${bench_type} ${store_types[${dev_id}]} ${devs[${dev_id}]} ${LOGPATH}
+        done
+    done
 done
