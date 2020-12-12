670bf1be8ed9aecd047b2327699d3f81190b2f18
wwyf <w.y.ff@163.com>
2020-12-12 14:24:20 +0800

    update

--- bench.sh
+++ bench.sh
@@ -9,19 +9,18 @@ devs=(/dev/sda /dev/sdb  /dev/nvme0n1)
 for dev in ${devs[*]}
 do
 
-# # TODO: error
-# # fio latency/fiorandomrwlatency.fio
-# fio --filename=${dev} --direct=1 --rw=randrw --bs=4k --ioengine=libaio --iodepth=1 --numjobs=1 --time_based --group_reporting --name=rwlatency-test-job --runtime=120 --eta-newline=1 --readonly
-# sync
-# echo 3 > /proc/sys/vm/drop_caches
-# sleep 5
-
 # fio latency/fiorandomreadlatency.fio
 fio --filename=${dev} --direct=1 --rw=randread --bs=4k --ioengine=libaio --iodepth=1 --numjobs=1 --time_based --group_reporting --name=readlatency-test-job --runtime=120 --eta-newline=1 --readonly
 sync
 echo 3 > /proc/sys/vm/drop_caches
 sleep 5
 
+
+# fio latency/fiorandomrwlatency.fio
+fio --filename=${dev} --direct=1 --rw=randrw --bs=4k --ioengine=libaio --iodepth=1 --numjobs=1 --time_based --group_reporting --name=rwlatency-test-job --runtime=120 --eta-newline=1 
+sync
+echo 3 > /proc/sys/vm/drop_caches
+sleep 5
 
 # fio bandwidth/fiorandomread.fio
 fio --filename=${dev} --direct=1 --rw=randread --bs=64k --ioengine=libaio --iodepth=64 --runtime=120 --numjobs=4 --time_based --group_reporting --name=throughput-test-job --eta-newline=1 --readonly
